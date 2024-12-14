1e22b67344aa51abf65fbba62b236eb2f76c383b
Jbishopdev <osrsironmangim@gmail.com>
2024-12-14 13:51:34 -0500

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversedString = "";
+  for (i = 0; i = string.length; i++){
+    reversedString += string[string.length - 1];
+    console.log(reversedString);
+    string = string.substring(0, string.length - 1);
+    console.log(string);
+  }
+  return reversedString;
 
 };
 
