3c94f6034f16f7f994a87b76b91ccf058fa9fb56
Jbishopdev <osrsironmangim@gmail.com>
2024-12-14 17:56:46 -0500

    Add exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+  let product = 0;
 
+
+  if (!Number.isInteger(numOne) || !Number.isInteger(numTwo) || numOne < 0 || numTwo < 0 || numOne === NaN || numTwo === NaN){
+    return "ERROR";
+  }
+  else if (numOne < numTwo) {
+    for (let i = numOne; i <= numTwo; i++) {
+      product += i;
+    }
+    return product;
+  }
+  else {
+    for (let i = numTwo; i <= numOne; i++) {
+      product += i;
+    }
+    return product;
+  }
 };
 
 // Do not edit below this line
