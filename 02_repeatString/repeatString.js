ab845c0da8306e9b0637cd5397208beabe83a43e
Jbishopdev <osrsironmangim@gmail.com>
2024-12-14 13:37:08 -0500

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  let i = 0;
+  let repeatedString = "";
+
+  while (i < num){
+    repeatedString += string;
+    i++
+  }
+  return repeatedString;
+  
 };
 
 // Do not edit below this line
